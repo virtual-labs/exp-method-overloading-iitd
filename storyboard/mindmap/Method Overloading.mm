fcfd1aaa225ee684bd0088458050472bf59262a9
vlabsdev-iitd <vlabsdev.iitd@gmail.com>
2021-02-25 19:49:55 +0530

    Exp5

---
+++ storyboard/mindmap/Method Overloading.mm
@@ -0,0 +1,92 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Method Overloading" FOLDED="false" ID="ID_1017333432" CREATED="1601295345443" MODIFIED="1601296059497" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Ways to perform method overloading" POSITION="right" ID="ID_1092037836" CREATED="1601295425577" MODIFIED="1601296059497" HGAP_QUANTITY="2.000000357627858 pt" VSHIFT_QUANTITY="-44.24999868124728 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Overloading by changing the number of arguments" ID="ID_1435494052" CREATED="1601295453419" MODIFIED="1601295469808"/>
+<node TEXT="By changing the datatype of parameters" ID="ID_1261503048" CREATED="1601295475154" MODIFIED="1601295487541"/>
+</node>
+<node TEXT="Syntax" POSITION="left" ID="ID_1244182915" CREATED="1601295658924" MODIFIED="1601295664346">
+<edge COLOR="#0000ff"/>
+<node TEXT="data types" ID="ID_1765338819" CREATED="1601295668227" MODIFIED="1601295677680"/>
+<node TEXT="variables" ID="ID_761126211" CREATED="1601295681732" MODIFIED="1601295690034"/>
+<node TEXT="mathods" ID="ID_829397129" CREATED="1601295694584" MODIFIED="1601295701557"/>
+<node TEXT="base class" ID="ID_1635929644" CREATED="1601295705830" MODIFIED="1601295712877"/>
+<node TEXT="Super class" ID="ID_1707048969" CREATED="1601295725667" MODIFIED="1601295735023"/>
+<node TEXT="Conditional Statement" ID="ID_278829436" CREATED="1601295746608" MODIFIED="1601295756192"/>
+<node TEXT="Loops" ID="ID_1030527530" CREATED="1601295761564" MODIFIED="1601295766885"/>
+<node TEXT="access specifier" ID="ID_1627047978" CREATED="1601295913298" MODIFIED="1601295926686"/>
+</node>
+<node TEXT="Advantage" POSITION="right" ID="ID_1764929899" CREATED="1601295932470" MODIFIED="1601296056445" HGAP_QUANTITY="-5.499999418854731 pt" VSHIFT_QUANTITY="75.74999774247415 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="Reduces execution time" ID="ID_1302373861" CREATED="1601295959308" MODIFIED="1601295977799"/>
+<node TEXT="Code reusability is achieved" ID="ID_1515707401" CREATED="1601295981323" MODIFIED="1601295998225"/>
+<node TEXT="Code complexity is reduced" ID="ID_472023827" CREATED="1601296000286" MODIFIED="1601296013201"/>
+<node TEXT="It saves memory." ID="ID_1186825367" CREATED="1601296016820" MODIFIED="1601296051797"/>
+<node TEXT="provides consistency of code" ID="ID_71227645" CREATED="1601296033841" MODIFIED="1601296045699"/>
+</node>
+</node>
+</map>
